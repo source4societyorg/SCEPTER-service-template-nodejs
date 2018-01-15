89cd3428debf05b458db4ed1ef8da7a3b0ab60ca
Daniel Fredriksen <dfredriksen@cyint.technology>
2018-01-15 02:31:51 -0800

    Corrected License to be MIT

--- test/services.test.js
+++ test/services.test.js
@@ -16,8 +16,8 @@ test('prepareErrorResponse returns the error output of the service', () => {
   let service = new HelloService('test1', './test/credentials-test.json', './test/parameters-test.json', './test/services-test.json')
   const mockError = { message: 'error', code: 500 }
   const mockError2 = 'error'
-  const expectedResponse = {body: '"error"', status: 500, statusCode: 500}
-  const expectedResponse2 = {body: '"Unexpected Error"', status: 500, statusCode: 500}
+  const expectedResponse = { body: '"error"', headers: { 'Content-Type': 'application/json' }, 'status': 500, 'statusCode': 500 }
+  const expectedResponse2 = { body: '"Unexpected Error"', headers: { 'Content-Type': 'application/json' }, status: 500, statusCode: 500 }
   expect(service.prepareErrorResponse(mockError)).toEqual(expectedResponse)
   expect(service.prepareErrorResponse(mockError2)).toEqual(expectedResponse)
   expect(service.prepareErrorResponse()).toEqual(expectedResponse2)
@@ -25,6 +25,6 @@ test('prepareErrorResponse returns the error output of the service', () => {
 
 test('prepareSuccessResponse returns "hello"', () => {
   let service = new HelloService('test1', './test/credentials-test.json', './test/parameters-test.json', './test/services-test.json')
-  const expectedResponse = {body: '"Hello"', status: 200, statusCode: 200}
+  const expectedResponse = { body: '"Hello"', headers: { 'Content-Type': 'application/json' }, status: 200, statusCode: 200 }
   expect(service.prepareSuccessResponse('Hello')).toEqual(expectedResponse)
 })
